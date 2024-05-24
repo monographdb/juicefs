1024116ded2b2bb4b4b75abe3b2199a6a0e7fff0
zhanghao <zhanghao997@qq.com>
2024-05-24 17:18:33 +0800

    metadata engine only benchmark

--- cmd/bench_meta.go
+++ cmd/bench_meta.go
@@ -22,13 +22,37 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/juicedata/juicefs/pkg/fs"
+	"github.com/juicedata/juicefs/pkg/meta"
 	"github.com/urfave/cli/v2"
 )
 
 func cmdMetaBench() *cli.Command {
+	selfFlags := []cli.Flag{
+		&cli.UintFlag{
+			Name:  "count",
+			Value: 100,
+			Usage: "operations per thread",
+		},
+		&cli.UintFlag{
+			Name:  "threads",
+			Value: 1,
+			Usage: "number of concurrent threads(goroutine)",
+		},
+		&cli.StringSliceFlag{
+			Name:  "steps",
+			Value: cli.NewStringSlice(stepCreate, stepStat, stepOpen, stepRemove),
+			Usage: "test suit steps",
+		},
+		&cli.StringFlag{
+			Name:  "url",
+			Usage: "metadata engine URL",
+		},
+	}
 	return &cli.Command{
 		Name:        "mdbench",
 		Action:      metadataBench,
@@ -36,59 +60,117 @@ func cmdMetaBench() *cli.Command {
 		Usage:       "Run metadata benchmarks on a path",
 		ArgsUsage:   "PATH",
 		Description: "benchmark for metadata",
-		Flags: []cli.Flag{
-			&cli.UintFlag{
-				Name:  "count",
-				Value: 100,
-				Usage: "operations per thread",
-			},
-			&cli.UintFlag{
-				Name:  "threads",
-				Value: 1,
-				Usage: "number of concurrent threads(goroutine)",
-			},
-			&cli.StringSliceFlag{
-				Name:  "steps",
-				Value: cli.NewStringSlice(stepCreate, stepStat, stepRead, stepRemove),
-				Usage: "operation kinds",
-			},
-		},
+		Flags:       expandFlags(selfFlags, clientFlags(0), shareInfoFlags()),
 	}
 }
+
+const (
+	stepCreate = "create"
+	stepStat   = "stat"
+	stepOpen   = "open"
+	stepRemove = "remove"
+)
 
 type MetaBench struct {
 	dir       string
 	threads   uint
 	reqs      uint
 	purgeArgs []string
+	funcs     map[string]func(string)
+	jfs       *fs.FileSystem
 }
 
-const (
-	stepCreate = "create"
-	stepStat   = "stat"
-	stepRead   = "read"
-	stepRemove = "remove"
-)
-
 func (b *MetaBench) prepare() {
-	if _, err := os.Stat(b.dir); os.IsNotExist(err) {
-		if err = os.MkdirAll(b.dir, os.ModePerm); err != nil {
-			logger.Fatalf("Failed to create %s: %s", b.dir, err)
+	if b.jfs == nil {
+		if _, err := os.Stat(b.dir); os.IsNotExist(err) {
+			if err = os.MkdirAll(b.dir, os.ModePerm); err != nil {
+				logger.Fatalf("Failed to create %s: %s", b.dir, err)
+			}
 		}
-	}
-	for i := uint(0); i < b.threads; i++ {
-		d := b.routine_dir(i)
-		if _, err := os.Stat(d); os.IsNotExist(err) {
-			err = os.Mkdir(d, os.ModePerm)
+		for i := uint(0); i < b.threads; i++ {
+			d := b.routine_dir(i)
+			if _, err := os.Stat(d); os.IsNotExist(err) {
+				if err = os.Mkdir(d, os.ModePerm); err != nil {
+					logger.Fatalf("Failed to create %s: %s", d, err)
+				}
+			}
+		}
+
+		b.funcs[stepCreate] = func(fn string) {
+			file, err := os.Create(fn)
+			if err != nil {
+				logger.Fatalf("Failed to create %s: %s", fn, err)
+			}
+			file.Close()
+		}
+		b.funcs[stepStat] = func(fn string) {
+			_, err := os.Stat(fn)
 			if err != nil {
 				panic(err)
+			}
+		}
+		b.funcs[stepOpen] = func(fn string) {
+			file, err := os.Open(fn)
+			if err != nil {
+				panic(err)
+			}
+			file.Close()
+		}
+		b.funcs[stepRemove] = func(fn string) {
+			err := os.Remove(fn)
+			if err != nil {
+				panic(err)
+			}
+		}
+	} else {
+		if _, err := b.jfs.Stat(ctx, b.dir); os.IsNotExist(err) {
+			if err = b.jfs.MkdirAll(ctx, b.dir, 0777, umask); err != 0 {
+				logger.Fatalf("Failed to create %s: %s", b.dir, err)
+			}
+		}
+		for i := uint(0); i < b.threads; i++ {
+			d := b.routine_dir(i)
+			if _, err := b.jfs.Stat(ctx, d); os.IsNotExist(err) {
+				if err = b.jfs.Mkdir(ctx, d, 0777, umask); err != 0 {
+					logger.Fatalf("Failed to create %s: %s", d, err)
+				}
+			}
+		}
+
+		b.funcs[stepCreate] = func(fn string) {
+			file, err := b.jfs.Create(ctx, fn, 0666, umask)
+			if err != 0 {
+				logger.Fatalf("Failed to create %s: %s", fn, err)
+			}
+			file.Close(ctx)
+		}
+		b.funcs[stepStat] = func(fn string) {
+			_, err := b.jfs.Stat(ctx, fn)
+			if err != 0 {
+				logger.Fatalf("Failed to stat %s: %s", fn, err)
+			}
+		}
+		b.funcs[stepOpen] = func(fn string) {
+			file, err := b.jfs.Open(ctx, fn, meta.MODE_MASK_R|meta.MODE_MASK_W)
+			if err != 0 {
+				logger.Fatalf("Failed to open %s: %s", fn, err)
+			}
+			file.Close(ctx)
+		}
+		b.funcs[stepRemove] = func(fn string) {
+			err := b.jfs.Delete(ctx, fn)
+			if err != 0 {
+				logger.Fatalf("Failed to delete %s: %s", fn, err)
 			}
 		}
 	}
 }
 
-func (b *MetaBench) run(desc string, f func(string)) {
-	b.dropCaches()
+func (b *MetaBench) run(step string) {
+	if b.jfs == nil {
+		b.dropCaches()
+	}
+	stepFunc := b.funcs[step]
 	wg := sync.WaitGroup{}
 	wg.Add(int(b.threads))
 	start := time.Now()
@@ -97,7 +179,7 @@ func (b *MetaBench) run(desc string, f func(string)) {
 			d := b.routine_dir(i)
 			for j := uint(0); j < b.reqs; j++ {
 				fn := b.filename(d, j)
-				f(fn)
+				stepFunc(fn)
 			}
 			wg.Done()
 		}(i)
@@ -106,7 +188,7 @@ func (b *MetaBench) run(desc string, f func(string)) {
 	cost := time.Since(start)
 	total := b.threads * b.reqs
 	ops := float64(total) / cost.Seconds()
-	logger.Printf("%s: %d operation cost %v: OPS=%f", desc, total, cost, ops)
+	logger.Infof("%s: %d operations, cost %v, OPS=%.2f", strings.ToUpper(step), total, cost, ops)
 }
 
 func (b *MetaBench) routine_dir(i uint) string {
@@ -148,49 +230,24 @@ func metadataBench(ctx *cli.Context) error {
 		logger.Fatal("Currently only support Linux/macOS")
 	}
 	if os.Getuid() != 0 {
-		fmt.Println("Clean kernel cache may ask for root privilege...")
+		logger.Infof("Clean kernel cache may ask for root privilege...")
 	}
 	bench := MetaBench{
 		dir:       mount_point,
 		threads:   threads,
 		reqs:      reqCnt,
 		purgeArgs: purgeArgs,
+		funcs:     make(map[string]func(string)),
+	}
+	metaUrl := ctx.String("url")
+	if metaUrl != "" {
+		jfs := initForMdtest(ctx, "mdbench", metaUrl)
+		bench.jfs = jfs
 	}
 	bench.prepare()
-
+	logger.Infof("metadata benchmark start...")
 	for _, step := range steps {
-		switch step {
-		case stepCreate:
-			bench.run(step, func(fn string) {
-				file, err := os.Create(fn)
-				if err != nil {
-					panic(err)
-				}
-				file.Close()
-			})
-		case stepStat:
-			bench.run(step, func(fn string) {
-				_, err := os.Stat(fn)
-				if err != nil {
-					panic(err)
-				}
-			})
-		case stepRead:
-			bench.run(step, func(fn string) {
-				file, err := os.Open(fn)
-				if err != nil {
-					panic(err)
-				}
-				file.Close()
-			})
-		case stepRemove:
-			bench.run(step, func(fn string) {
-				err := os.Remove(fn)
-				if err != nil {
-					panic(err)
-				}
-			})
-		}
+		bench.run(step)
 	}
 	return nil
 }
